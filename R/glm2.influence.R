cedb4647ebdbdf17c41c92a97632022666681a27
Mark Donoghoe <markdonoghoe@gmail.com>
2025-08-08 16:25:07 +1000

    Implement influence.glm2Matrix
    Working version of influence() for glm2Matrix objects

--- R/glm2.influence.R
+++ R/glm2.influence.R
@@ -40,8 +40,136 @@
 
 influence.glm2Matrix <- function(model, do.coef = TRUE, ...) {
   
-  stop("influence measures not implemented for glm.fit2.Matrix")
+  #stop("influence measures not implemented for glm.fit2.Matrix")
+  res <- lm.influence.Matrix(model, do.coef = do.coef, ...)
+  pRes <- na.omit(residuals(model, type = "pearson"))[model$prior.weights != 0]
+  pRes <- naresid(model$na.action, pRes)
+  names(res)[names(res) == "wt.res"] <- "dev.res"
+  c(res, list(pear.res = pRes))
   
+}
+
+lm.influence.Matrix <- function(model, do.coef = TRUE) {
+
+  # TODO: Improve on this to use Matrix instead of matrix
+  wt.res <- weighted.residuals(model)
+  e <- na.omit(wt.res)
+  is.mlm <- is.matrix(e) # n x q  matrix in the mulitvariate lm case
+  if (model$rank == 0) {
+    n <- length(wt.res) # drops 0 wt, may drop NAs
+    sigma <- sqrt(deviance(model) / df.residual(model))
+    res <- list(hat = rep(0, n), coefficients = matrix(0, n, 0),
+                sigma = rep(sigma, n))
+  } else {
+    ## if we have a point with hat = 1, the corresponding e should be
+    ## exactly zero.  Protect against returning Inf by forcing this
+    e[abs(e) < 100 * .Machine$double.eps * median(abs(e))] <- 0
+    mqr <- model$qr_full
+    n <- as.integer(nrow(mqr$qr))
+    if (is.na(n)) stop("invalid model QR matrix")
+    ## in na.exclude case, omit NAs; also drop 0-weight cases
+    if(NROW(e) != n)
+      stop("non-NA residual length does not match cases used in fitting")
+    do.coef <- as.logical(do.coef)
+    tol <- 10 * .Machine$double.eps
+    ## original code:
+    ## res <- .Call(C_influence, mqr, e, tol)
+    res <- influence.Matrix(mqr, e, tol)
+    if (do.coef) {
+      ok <- seq_len(Matrix::qr2rankMatrix(mqr$qr)) # need this for rank-deficient cases
+      Q <- Matrix::qr.Q(mqr$qr)[, ok, drop=FALSE]
+      R <- Matrix::qr.R(mqr$qr)[ok, ok, drop=FALSE]
+      hat <- res$hat
+      invRQtt <- Matrix::t(Matrix::solve(R, Matrix::t(Q)))
+      # Need to back-permute this
+      qr.uns <- is.unsorted(mqr$pivot, strictly = TRUE)
+      if (qr.uns) invRQtt <- invRQtt[, Matrix::invertPerm(mqr$pivot)]
+      k <- NCOL(Q)
+      q <- NCOL(e)
+      ## NB: The following relies on recycling: diag(v) %*% A == A * v
+      ## so we need a for loop for the mlm case
+      res$coefficients <-
+        if(is.mlm) {
+          cf <- array(0, c(n,k,q))
+          for (j in seq_len(q))
+            cf[,,j] <- invRQtt * ifelse(hat == 1, 0, e[,j]/(1 - hat))
+          cf
+        } else
+          invRQtt * ifelse(hat == 1, 0, e/(1 - hat))
+    }
+
+    drop1d <- function(a) { # more cautious variant of drop(.)
+      d <- dim(a)
+      if(length(d) == 3L && d[[3L]] == 1L)
+        dim(a) <- d[-3L]
+      a
+    }
+    if (is.null(model$na.action)) {
+      if (!is.mlm) {  ## drop the 'q=1' array extent (from C)
+        res$sigma <- drop(res$sigma)
+        if (do.coef)
+          res$coefficients <- drop1d(res$coefficients)
+      }
+    } else {
+      hat <- naresid(model$na.action, res$hat)
+      hat[is.na(hat)] <- 0      # omitted cases have 0 leverage
+      res$hat <- hat
+      if (do.coef) {
+        coefficients <- naresid(model$na.action, res$coefficients)
+        coefficients[is.na(coefficients)] <- 0 # omitted cases have 0 change
+        res$coefficients <- if(is.mlm) coefficients else drop1d(coefficients)
+      }
+      sigma <- naresid(model$na.action, res$sigma)
+      sigma[is.na(sigma)] <- sqrt(deviance(model)/df.residual(model))
+      res$sigma <- if(is.mlm) sigma else drop(sigma)
+    }
+  }
+  res$wt.res <- naresid(model$na.action, e)
+  res$hat[res$hat > 1 - 10*.Machine$double.eps] <- 1 # force 1
+  names(res$hat) <- names(res$sigma) <- names(res$wt.res)
+  if(do.coef) {
+	  cf <- coef(model)
+	  if(is.mlm) { # coef is 3d array
+	    dnr <- dimnames(res$wt.res)
+	    dimnames(res$coefficients) <- list(
+		    dnr[[1L]],
+		    rownames(cf)[!apply(cf, 1L, anyNA)],
+		    dnr[[2L]])
+	  } else {
+	    dimnames(res$coefficients) <- list(names(res$wt.res),
+					                               names(cf)[!is.na(cf)])
+	  }
+  }
+  res[c("hat", "coefficients", "sigma", "wt.res")] # ensure order, for backward compatibility and regression tests
+}
+
+influence.Matrix <- function(mqr, e, tol) {
+
+  # Diagonal of hat matrix
+  Q <- Matrix::qr.Q(mqr$qr)
+  h <- Matrix::rowSums(Q^2)
+  h[h >= 1 - tol] <- 1
+
+  # Estimated residual standard deviation
+  n <- nrow(mqr$qr)
+  k <- Matrix::qr2rankMatrix(mqr$qr)
+  
+  is.mlm <- is.matrix(e)
+  q <- NCOL(e)
+  e <- matrix(e, nrow = n, ncol = q)
+
+  denom <- (n - k - 1)
+  e2sum <- colSums(e^2)
+  sigma <- matrix(sqrt(e2sum / denom), nrow = n, ncol = q, byrow = TRUE)
+  
+  hk <- h < 1
+  # sigma[i,j] = sqrt((e2sum[j] - e[i,j]^2 / (1 - h[i])) / denom)
+  s1 <- sweep(e[hk,,drop=FALSE]^2, 1, 1 - h[hk], FUN = "/")
+  s2 <- sweep(-s1, 2, e2sum, FUN = "+")
+  sigma[hk,] <- sqrt(s2 / denom)
+
+  list(hat = h, sigma = sigma)
+
 }
 
 #' Regression Deletion Diagnostics (not yet implemented)
